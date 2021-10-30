7b18054575d4d4ab7023e2c7a79b83e053a07287
iiyama <kdricemt@gmail.com>
2021-10-30 16:57:18 -0700

    update

--- Problem_1/cam_calibrator.py
+++ Problem_1/cam_calibrator.py
@@ -150,7 +150,7 @@ class CameraCalibrator:
         x_T = vh[idx_min,:]   # 1x9
         print(x_T)
         print(x_T.T)  # 9x1
-        H = np.hstack((x_T[0:3].T, x_T[3:6].T, x_T[6:9].T)) # 3x3
+        H = np.vstack((x_T[0:3].T, x_T[3:6].T, x_T[6:9].T)) # 3x3
 
         print("----------")
         print("H:")
