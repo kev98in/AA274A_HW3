2a5708c540616778949d965487ec27c778086c13
iiyama <kdricemt@gmail.com>
2021-11-13 19:03:42 -0800

    compute innovations

--- HW4/validate_ekf.py
+++ HW4/validate_ekf.py
@@ -319,7 +319,7 @@ if __name__ == '__main__':
     ## Subcomponent validation
     validate_localization_transition_model()
     validate_localization_compute_predicted_measurements()
-    #validate_localization_compute_innovations()
+    validate_localization_compute_innovations()
 
     ### PROBLEM 2
 
